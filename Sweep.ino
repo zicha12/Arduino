b9c008205ddda1b4b2af0e8735f5c676e3355679
zicha12 <62481071+zicha12@users.noreply.github.com>
2020-03-21 20:51:09 +0200

    Update Sweep.ino

--- Sweep.ino
+++ Sweep.ino
@@ -15,7 +15,7 @@ void loop() {
   for (int timeValue = 0 ; timeValue <= 255; timeValue += 5) {
     // sets the value (range from 0 to 255):
     analogWrite(Tran, 200);
-    // wait for 30 milliseconds to see the dimming effect
+    
     delay(timeValue);
     analogWrite(Tran, 0);
     delay(30);
